f5687022a98c4127a94cc2001cb743949e15bb55
nickh2000 <nickhurley2000@gmail.com>
2023-01-18 00:12:57 +0100

    Fixed relative paths in condor_submit

--- condor_submit.py
+++ condor_submit.py
@@ -25,7 +25,7 @@ submit += 'error = submit01.err\n'
 submit += 'log = submit01.log\n'
 submit += '+JobFlavour = "microcentury"\n' 
 submit += 'queue\n'
-submitName = "plots/tmp/" + args.input + '.sub'
+submitName = args.input + '.sub'
 sub1 = open(submitName,'w')
 sub1.write(submit+'\n')
 sub1.close() ##finish writing .sub file
@@ -41,8 +41,8 @@ for i in range(int(num_jobs)):
     create += 'cd /afs/cern.ch/user/n/nhurley/EMTFAnalyzer/AWBTools/macros/ \n' ## go to the src directly 
 
     #Run python script
-    create += 'python ' + args.input + ' -n ' + args.num_jobs + ' -i ' + str(i) + ' \n'
-    createName = 'plots/tmp/submit'+str(i)+'.sh' #Job's submission file output
+    create += 'python3 ' + args.input + ' -n ' + args.num_jobs + ' -i ' + str(i) + ' \n'
+    createName = '/afs/cern.ch/user/n/nhurley/EMTFAnalyzer/AWBTools/macros/plots/tmp/submit'+str(i)+'.sh' #Job's submission file output
 
     sub2 = open(createName,'w')
     sub2.write(create+'\n')
--- plots/tmp/submit0.sh
+++ plots/tmp/submit0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CMSSW_PROJECT_SRC=/afs/cern.ch/user/n/nhurley/CMSSW_12_4_6_emtfReEmul/src
 cd /afs/cern.ch/user/n/nhurley/EMTFAnalyzer/AWBTools/macros/ 
-python 2018compare.py -n 50 -i 0 
+python3 BDT.py -n 1 -i 0 
 
